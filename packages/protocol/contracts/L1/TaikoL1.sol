49692c2522bbb03b6437a5124cf6510555eb3db8
pingke <pingke@quarkchain.org>
2024-08-29 12:02:32 +0800

    fix

--- packages/protocol/contracts/L1/TaikoL1.sol
+++ packages/protocol/contracts/L1/TaikoL1.sol
@@ -84,7 +84,7 @@ contract TaikoL1 is EssentialContract, ITaikoL1, TaikoEvents {
     {
         TaikoData.Config memory config = getConfig();
 
-        (meta_,, deposits_) = LibProposing.proposeBlock(state, config, this, address(0), _params, _txList);
+        (meta_,, deposits_) = LibProposing.proposeBlock(state, config, this, _params, _txList);
         if (meta_.id >= config.ontakeForkHeight) revert L1_FORK_ERROR();
 
         if (LibUtils.shouldVerifyBlocks(config, meta_.id, true) && !state.slotB.provingPaused) {
@@ -334,7 +334,7 @@ contract TaikoL1 is EssentialContract, ITaikoL1, TaikoEvents {
         internal
         returns (TaikoData.BlockMetadataV2 memory meta_)
     {
-        (, meta_,) = LibProposing.proposeBlock(state, _config, this, address(0), _params, _txList);
+        (, meta_,) = LibProposing.proposeBlock(state, _config, this, _params, _txList);
         if (meta_.id < _config.ontakeForkHeight) revert L1_FORK_ERROR();
 
         if (LibUtils.shouldVerifyBlocks(_config, meta_.id, true) && !state.slotB.provingPaused) {
--- packages/protocol/contracts/hekla/HeklaTaikoL1.sol
+++ packages/protocol/contracts/hekla/HeklaTaikoL1.sol
@@ -27,7 +27,8 @@ contract HeklaTaikoL1 is TaikoL1 {
                 minGasExcess: 1_340_000_000,
                 maxGasIssuancePerBlock: 600_000_000 // two minutes
              }),
-            ontakeForkHeight: 720_000 // = 7200 * 100
+            ontakeForkHeight: 720_000, // = 7200 * 100
+            storageContract: address(0)
          });
     }
 }
