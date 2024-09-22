cc77d7ac3a01ca4135413edb553e9e85109d50d7
pingke <pingke@quarkchain.org>
2024-09-22 20:10:17 +0800

    fxi

--- packages/protocol/contracts/devnet/DevnetTaikoL1.sol
+++ packages/protocol/contracts/devnet/DevnetTaikoL1.sol
@@ -10,7 +10,7 @@ contract DevnetTaikoL1 is TaikoL1 {
     /// @inheritdoc ITaikoL1
     function getConfig() public pure override returns (TaikoData.Config memory) {
         return TaikoData.Config({
-            chainId: LibNetwork.TAIKO_ESTAIKO,
+            chainId: 167_001,
             blockMaxProposals: 324_000,
             blockRingBufferSize: 360_000,
             maxBlocksToVerify: 16,
