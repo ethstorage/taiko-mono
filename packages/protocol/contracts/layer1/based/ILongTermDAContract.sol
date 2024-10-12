01116b6c7dad78aec74fad373c789263c50cbfad
pingke <pingke@quarkchain.org>
2024-10-12 17:48:51 +0800

    fix

--- packages/protocol/contracts/layer1/based/ILongTermDAContract.sol
+++ packages/protocol/contracts/layer1/based/ILongTermDAContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 interface ILongTermDAContract {
     function putBlob(bytes32 key, uint256 blobIdx, uint256 length) external payable;
